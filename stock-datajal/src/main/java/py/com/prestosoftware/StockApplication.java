56e143e1922c7bd1b90b2925291fad0a585952c8
Hugo Denis Rolon <hugo.rolon@pti.org.py>
2021-12-29 17:37:39 -0300

    Instalacion agro progreso 29122021

--- stock-datajal/src/main/java/py/com/prestosoftware/StockApplication.java
+++ stock-datajal/src/main/java/py/com/prestosoftware/StockApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import py.com.prestosoftware.ui.main.MainController;
 
@@ -13,9 +15,12 @@ import py.com.prestosoftware.ui.main.MainController;
 @ComponentScan
 public class StockApplication  {
 
+	private static final Logger LOGGER=LoggerFactory.getLogger(StockApplication.class);
+	
 	public static void main(String[] args) {
         ConfigurableApplicationContext context = new SpringApplicationBuilder(StockApplication.class).headless(false).run(args);
         MainController mainMenuController = context.getBean(MainController.class);
         mainMenuController.prepareAndOpenFrame();
+        LOGGER.info("Simple log statement with inputs {}, {} and {}", 1,2,3);
 	}	
 }
---
+++ stock-datajal/stock.bat
@@ -0,0 +1,1 @@
+java -jar target/stock-agroprogreso-0.0.1.jar
