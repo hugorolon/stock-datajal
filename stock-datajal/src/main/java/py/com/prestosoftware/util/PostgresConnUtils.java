fc9c179a6e8cb90cc95d1ba3b36d0fea4c7c9579
Hugo Denis Rolon <hugo.rolon@pti.org.py>
2022-01-06 23:22:04 -0300

    Pruebas con base de datos

--- stock-datajal/src/main/java/py/com/prestosoftware/util/PostgresConnUtils.java
+++ stock-datajal/src/main/java/py/com/prestosoftware/util/PostgresConnUtils.java
@@ -9,7 +9,7 @@ public class PostgresConnUtils {
     public static Connection getPostgresConnection()
             throws ClassNotFoundException, SQLException {
         String hostName = "server";
-        String dbName = "sys_agroprogreso07122021";
+        String dbName = "sys_agroprogreso30122021";
         String userName = "postgres";
         String password = "adminpg";
         return getPostgresConnection(hostName, dbName, userName, password);
